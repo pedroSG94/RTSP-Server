0518bfbaf484a3bacedc19b019269d4b886f8231
pedro <pedrosanchez@droiders.com>
2019-04-03 14:24:39 +0200

    add others modes

---
+++ app/src/main/java/com/pedro/rtspserver/rtsp/RtspServerFromFile.kt
@@ -0,0 +1,125 @@
+package com.pedro.rtspserver.rtsp
+
+import android.content.Context
+import android.media.MediaCodec
+import android.os.Build
+import android.support.annotation.RequiresApi
+import com.pedro.encoder.input.decoder.AudioDecoderInterface
+import com.pedro.encoder.input.decoder.VideoDecoderInterface
+import com.pedro.encoder.utils.CodecUtil
+import com.pedro.rtplibrary.base.FromFileBase
+import com.pedro.rtplibrary.view.LightOpenGlView
+import com.pedro.rtplibrary.view.OpenGlView
+import com.pedro.rtsp.rtsp.VideoCodec
+import com.pedro.rtsp.utils.ConnectCheckerRtsp
+import java.nio.ByteBuffer
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+class RtspServerFromFile : FromFileBase {
+
+  private val rtspServer: RtspServer
+
+  constructor(openGlView: OpenGlView, connectCheckerRtsp: ConnectCheckerRtsp, port: Int,
+    videoDecoderInterface: VideoDecoderInterface, audioDecoderInterface: AudioDecoderInterface)
+      : super(openGlView, videoDecoderInterface, audioDecoderInterface) {
+    rtspServer = RtspServer(openGlView.context, connectCheckerRtsp, port)
+  }
+
+  constructor(lightOpenGlView: LightOpenGlView, connectCheckerRtsp: ConnectCheckerRtsp, port: Int,
+    videoDecoderInterface: VideoDecoderInterface, audioDecoderInterface: AudioDecoderInterface)
+      : super(lightOpenGlView, videoDecoderInterface, audioDecoderInterface) {
+    rtspServer = RtspServer(lightOpenGlView.context, connectCheckerRtsp, port)
+  }
+
+  constructor(context: Context, connectCheckerRtsp: ConnectCheckerRtsp, port: Int,
+    videoDecoderInterface: VideoDecoderInterface, audioDecoderInterface: AudioDecoderInterface)
+      : super(context, videoDecoderInterface, audioDecoderInterface) {
+    rtspServer = RtspServer(context, connectCheckerRtsp, port)
+  }
+
+  fun setVideoCodec(videoCodec: VideoCodec) {
+    videoEncoder.type =
+      if (videoCodec == VideoCodec.H265) CodecUtil.H265_MIME else CodecUtil.H264_MIME
+  }
+
+  fun getEndPointConnection(): String = "rtsp://${rtspServer.serverIp}:${rtspServer.port}/"
+
+  override fun setAuthorization(user: String, password: String) {
+    //not developed
+  }
+
+  fun startStream() {
+    super.startStream("")
+  }
+
+  override fun prepareAudioRtp(isStereo: Boolean, sampleRate: Int) {
+    rtspServer.isStereo = isStereo
+    rtspServer.sampleRate = sampleRate
+  }
+
+  override fun startStreamRtp(url: String) {
+    //unused
+  }
+
+  override fun stopStreamRtp() {
+    rtspServer.stopServer()
+  }
+
+  override fun getAacDataRtp(aacBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+    rtspServer.sendAudio(aacBuffer, info)
+  }
+
+  override fun onSpsPpsVpsRtp(sps: ByteBuffer, pps: ByteBuffer, vps: ByteBuffer?) {
+    val newSps = sps.duplicate()
+    val newPps = pps.duplicate()
+    val newVps = vps?.duplicate()
+    rtspServer.setVideoInfo(newSps, newPps, newVps)
+    rtspServer.startServer()
+  }
+
+  override fun getH264DataRtp(h264Buffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+    rtspServer.sendVideo(h264Buffer, info)
+  }
+
+  /**
+   * Unused functions
+   */
+  @Throws(RuntimeException::class) override fun resizeCache(newSize: Int) {
+  }
+
+  override fun getCacheSize(): Int {
+    return 0
+  }
+
+  override fun getSentAudioFrames(): Long {
+    return 0
+  }
+
+  override fun getSentVideoFrames(): Long {
+    return 0
+  }
+
+  override fun getDroppedAudioFrames(): Long {
+    return 0
+  }
+
+  override fun getDroppedVideoFrames(): Long {
+    return 0
+  }
+
+  override fun resetSentAudioFrames() {
+
+  }
+
+  override fun resetSentVideoFrames() {
+
+  }
+
+  override fun resetDroppedAudioFrames() {
+
+  }
+
+  override fun resetDroppedVideoFrames() {
+
+  }
+}
