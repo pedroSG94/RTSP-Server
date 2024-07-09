d7a61eec3777f9c39e95099b514af59825b633fe
pedroSG94 <pedrossgg1994@gmail.com>
2024-07-09 19:40:27 +0200

    migrate to version catalogs and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-val libraryGroup by rootProject.extra { "com.github.pedroSG94" }
-val vCode by rootProject.extra { 128 }
-val vName by rootProject.extra { "1.2.8" }
-val coroutinesVersion by rootProject.extra { "1.7.3" }
-val rootEncoderVersion by rootProject.extra { "2.4.5" }
 
 plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.dokka") version "1.9.20" apply true
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin) apply false
+    alias(libs.plugins.jetbrains.dokka) apply true
 }
 
 tasks.register("clean") {
